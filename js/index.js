3d3bc9467614f65c6351a6ad096381a249cb0813
Brian Dowd <dowdbrian@yahoo.com>
2021-03-11 18:10:33 +0100

    Done. Iteration 1-3. Plan to do bonus iterations later.

--- js/index.js
+++ js/index.js
@@ -2,24 +2,36 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  // const price = product.querySelector('.price span');
+  // above is code provided in lab. Where is 'product.' coming from? Shouldn't it be 'document.'?
+  const price = document.querySelector('.price span');
+  let quantity = document.querySelector('.quantity input'); 
+  let calculateSubtotal = price.innerHTML * quantity.innherHTML;
+  let subTotal = document.querySelector('.subtotal span');
+  subTotal.value = calculateSubtotal;
+  return calculateSubtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let product = document.getElementsByClassName('product');
+  updateSubtotal(product);
 
   // ITERATION 3
-  //... your code goes here
+  let subtotal = document.getElementsByClassName('subtotal');
+  let total = 0;
+    for (let i = 0; i < subtotal.length; i++) {
+      total += subtotal[i];
+    }
+  let totalValue = document.querySelector('.total-value span');
+  totalValue.innerHTML = total;
 }
-
 // ITERATION 4
 
 function removeProduct(event) {
